39d5a1a8d63526e7a376c09b81d7dc2ee0126a35
tuki <hack.localhost@gmx.de>
2013-06-18 02:26:00 +0200

    start implement to update events

---
+++ JS/kurseaendern/getmyevents.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2013 UniCopa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+* Give a rightholder his events back who he have the rights to change something
+*/
+
+/*
+*{"type":"GetMyEventsResponse","data":{"eventIDs":{"RIGHTHOLDER":[],"DEPUTY":[],"OWNER":[]}}}
+*/
+
+
+data_send=new Object();
+data_receive=new Object();
+
+
+data_send={type:"GetMyEventsRequest",data:{}}; //bauen des js Objekt
+data_receive=sendrequest(data_send);    //aufruf sendrequest in sendrequest.js
+
+if(data_receive.type!="GetMyEventsResponse"){
+    alert("Ops, something is not working, please try it again later!")
+}
+else{
+    var eventIDs=data_receive.data.eventIDs.RIGHTHOLDER;        //IMPORTANT: !!!!!! noch nach den Eigner-klassen unterscheiden, im moment nur fuer rightholder
+
+    for(e in eventIDs){
+        
+        var id=e;
+        var datum="00.00.0000"; //Noch auslesen
+        var uhrzeit="00:00";	//Noch auslesen
+        var raum="Raum";		//Noch auslesen
+        var lAE ="2013.03.12 <br/>11:46Uhr";	//Noch auslesen bzw. komplett weglassen
+        data_send={type:"GetEventRequest",data:{"eventID":e}}; 
+        data_receive=sendrequest(data_send);
+        
+        if(data_receive.type!="RequestNotPracticableException"){
+            var artVeranstaltung=data_receive.data.event.eventName;
+            var eventGroupID=data_receive.data.event.eventGroupID;
+            
+            data_send={type:"GetEventGroupRequest",data:{"eventGroupID":eventGroupID}}; 
+            data_receive=sendrequest(data_send);
+            
+            var name=data_receive.data.eventGroup.eventGroupName+" "+artVeranstaltung;
+            
+            var elem= "<tr id=\""+key+"\">";
+            elem+="<td><a href=\"#\"><img src=\"images/bearbeiten.png\"/></a></td>";
+            elem+="<td style=\"background-color:"+color+";\"></td>";
+            elem+="<td><a href=\"#\" class=\"linkToSubpage\">"+name+"</a></td>";
+            elem+="<td>"+datum+"</td>";
+            elem+="<td>"+uhrzeit+"</td>";
+            elem+="<td>"+raum+"</td>";
+            elem+="<td>"+lAE+"</td>";
+            elem+="</tr>";
+            
+            $('#kurse-aendern').append(elem);   //an Tabelle anhaengen
+        }
+        else{
+            alert("Event mit id="+key+" nicht vorhanden!");
+        }
+    }
+    alert("run correct");
+} 
---
+++ JS/kurseaendern/updateevent.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2013 UniCopa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+data_send=new Object();
+data_receive=new Object();
+
+//data_send={type:"AddSingleEventUpdateRequest",data:{updatedEvent:{updatedSingleEvent:"1",comment:"Seminarleiter ist Krank",latestLastUpdate:"-128"}}}; //bauen des js Objekt
+data_send={type:"AddSingleEventUpdateRequest",data:{updatedSingleEvent:"1",comment:"Seminarleiter ist Krank"}}; //bauen des js Objekt
+
+data_receive=sendrequest(data_send);    //aufruf sendrequest in sendrequest.js
+
+if(data_receive.type!="AddSingleEventUpdateResponse"){
+    alert("Ops, something is not working, please try it again later!")
+}
+else{
+    $('.linkToSubpage').on('click', function(){
+        var id=$(this).parent().parent().attr('id');
+        $('#inhalt').find('table').remove(); //Löscht aktuellen Ihnalt der Seite
+        
+        
+        data_send=new Object();
+        data_receive=new Object();
+        
+        data_send={type:"GetSingleEventRequest",data:{"id":id}}; //bauen des js Objekt
+        data_receive=sendrequest(data_send);
+        //SingleEvents ausgeben
+        
+        var newContent="<table  id=\"meineabos\">";
+        newContent+="<tr id=\"description\"><th>Datum</th><th>Uhrzeit</th><th>Raum</th><th>Zuletzt ge&auml;ndert</th><th>Von</th><th>Kommentar</th></tr><tr><td>14.06.2013</td><td>11:00</td><td>LdV HS 2</td><td>2013.06.08 <br/>23:53Uhr</td><td>Kuske</td><td>Raum- und Termin&auml;um&auml;nderung wegen ISWI</td></tr>";
+        newContent+="</table>";
+        $('#inhalt').append(newContent);
+    });
+
+}
